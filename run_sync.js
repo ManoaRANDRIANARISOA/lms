64f8f9a8b0a1d6af6c4c16e35b9bcfe9bd1cc9c9
unknown <manoafahendrena.randrianarisoa@gmail.com>
2026-07-10 12:38:09 +0300

    chore: nettoyage des fichiers de test et scripts temporaires

--- run_sync.js
+++
@@ -1,7 +0,0 @@
-const { syncWithCloud } = require('./out/main/services/sync.service.js')
-async function run() {
-  console.log('Starting sync...')
-  const res = await syncWithCloud()
-  console.log('Sync result:', res)
-}
-run()
